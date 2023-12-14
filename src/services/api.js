06dd0ced3f150b55d7f2c77b6e0901ff7334070d
shahruhabd <abdugapar04@gmail.com>
2023-12-15 01:23:26 +0600

    функция async для api

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,20 @@
+import axios from 'axios';
+
 const apiUrl = 'https://fakestoreapi.com';
 
 const ApiService = {
   getProducts: async () => {
-    const response = await fetch(`${apiUrl}/products`);
-    const data = await response.json();
-    return data;
+    try {
+      const response = await axios.get(`${apiUrl}/products`);
+      const transformedData = response.data.map((product) => ({
+        ...product,
+        formattedPrice: `$${product.price.toFixed(2)}`,
+      }));
+      return transformedData;
+    } catch (error) {
+      console.error('Error fetching and transforming products:', error);
+      throw error;
+    }
   },
 };
 
