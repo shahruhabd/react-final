f372d008b457b10b6c424beee24eea2787ecb313
shahruhabd <abdugapar04@gmail.com>
2023-12-14 22:45:52 +0600

    Добавлены обработчики события onClick и onKeyDown

--- src/pages/AddItem.js
+++ src/pages/AddItem.js
@@ -1,49 +1,60 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import '../css/main.css'
-
+import "../css/main.css";
 
 const AddItem = ({ onAddProduct }) => {
-    const [productName, setProductName] = useState('');
-    const [productPrice, setProductPrice] = useState('');
-  
-    const handleSubmit = (e) => {
-      e.preventDefault();
-  
-      const newProduct = {
-        id: Date.now(), 
-        name: productName,
-        price: parseFloat(productPrice),
-      };
-  
-      onAddProduct(newProduct);
-  
-      setProductName('');
-      setProductPrice('');
+  const [productName, setProductName] = useState("");
+  const [productPrice, setProductPrice] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newProduct = {
+      id: Date.now(),
+      name: productName,
+      price: parseFloat(productPrice),
     };
+
+    onAddProduct(newProduct);
+
+    setProductName("");
+    setProductPrice("");
+  };
   
-    return (
-      <form onSubmit={handleSubmit}>
-        <h3>Добавить новый товар</h3>
-        <label>
-          Название товара:
-          <input
-            type="text"
-            value={productName}
-            onChange={(e) => setProductName(e.target.value)}
-          />
-        </label>
-        <label>
-          Цена товара:
-          <input
-            type="number"
-            value={productPrice}
-            onChange={(e) => setProductPrice(e.target.value)}
-          />
-        </label>
-        <button type="submit">Добавить товар</button>
-      </form>
-    );
+  const handleButtonClick = () => {
+    console.log('Кнопка "Добавить товар" была нажата!');
   };
 
-export default AddItem
+  const handleKeyDown = (e) => {
+    console.log(`Клавиша ${e.key} была нажата в поле ввода`);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h3>Добавить новый товар</h3>
+      <label>
+        Название товара:
+        <input
+          type="text"
+          value={productName}
+          onChange={(e) => setProductName(e.target.value)}
+          onKeyDown={handleKeyDown}
+        />
+      </label>
+      <label>
+        Цена товара:
+        <input
+          type="number"
+          value={productPrice}
+          onChange={(e) => setProductPrice(e.target.value)}
+          onKeyDown={handleKeyDown}
+        />
+      </label>
+      <button type="submit" onClick={handleButtonClick}>
+        Добавить товар
+      </button>
+    </form>
+  );
+};
+
+export default AddItem;
