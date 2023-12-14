8a8ab56e7b2675e00e4890350b4d6c7e64981ce4
shahruhabd <abdugapar04@gmail.com>
2023-12-15 01:49:39 +0600

    Error boundary

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,6 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+const ErrorBoundary = ({ children }) => {
+  const [hasError, setHasError] = useState(false);
+
+  useEffect(() => {
+    const componentDidCatch = (error, errorInfo) => {
+      console.error('Error caught by error boundary:', error, errorInfo);
+      setHasError(true);
+    };
+
+    window.addEventListener('error', componentDidCatch);
+
+    return () => {
+      window.removeEventListener('error', componentDidCatch);
+    };
+  }, []);
+
+  return hasError ? (
+    <div>
+      <h2>Something went wrong!</h2>
+    </div>
+  ) : (
+    <>{children}</>
+  );
+};
+
 const Products = () => {
   const [products, setProducts] = useState([]);
 
@@ -28,16 +53,18 @@ const Products = () => {
   }, [products]);
 
   return (
-    <div>
-      <h2>Product List (Functional Component with useEffect)</h2>
-      <ul>
-        {products.map((product) => (
-          <li key={product.id}>
-            {product.title} - ${product.price.toFixed(2)}
-          </li>
-        ))}
-      </ul>
-    </div>
+    <ErrorBoundary>
+      <div>
+        <h2>Product List (Functional Component with useEffect)</h2>
+        <ul>
+          {products.map((product) => (
+            <li key={product.id}>
+              {product.title} - ${product.price.toFixed(2)}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </ErrorBoundary>
   );
 };
 
