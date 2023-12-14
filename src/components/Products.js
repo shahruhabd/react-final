de31310d6f198a930d11bda656cdd281a0b4278f
shahruhabd <abdugapar04@gmail.com>
2023-12-15 01:44:10 +0600

    Жизненный цикр компонентов

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,41 +1,44 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-class Products extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      products: [],
+const Products = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get('https://fakestoreapi.com/products');
+        setProducts(response.data);
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      }
     };
-  }
 
-  componentDidMount() {
-    this.fetchProducts();
-  }
+    fetchProducts();
 
-  fetchProducts = async () => {
-    try {
-      const response = await axios.get('https://fakestoreapi.com/products');
-      this.setState({ products: response.data });
-    } catch (error) {
-      console.error('Error fetching products:', error);
-    }
-  };
+    return () => {
+      console.log('Component is unmounting');
+    };
 
-  render() {
-    return (
-      <div>
-        <h2>Product List (Class Component)</h2>
-        <ul>
-          {this.state.products.map((product) => (
-            <li key={product.id}>
-              {product.title} - ${product.price.toFixed(2)}
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
-}
+  }, []); 
+
+  useEffect(() => {
+    console.log('Component has been updated:', products);
+
+  }, [products]);
+
+  return (
+    <div>
+      <h2>Product List (Functional Component with useEffect)</h2>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            {product.title} - ${product.price.toFixed(2)}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default Products;
